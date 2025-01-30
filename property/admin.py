eaaac68721d0635f45866cea5eea32d99e4e7d9a
yelena0000 <kirillnek07081999@gmail.com>
2025-01-31 02:12:37 +0300

    Update admin.py

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from .models import Flat, Complaint, Owner
 
-from .models import Flat, Complaint
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = (
+        'full_name',
+        'phone_number',
+        'normalized_phone',
+        'display_owned_flats'
+    )
+    search_fields = ('full_name', 'phone_number')
+    raw_id_fields = ('flats',)
+
+    def display_owned_flats(self, obj):
+        return obj.owned_flats()
+
+    display_owned_flats.short_description = "Квартиры в собственности"
 
 
 @admin.register(Flat)
@@ -8,42 +24,32 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = (
         'town',
         'address',
-        'owner',
-        'owner_pure_phone'
+        'owners__full_name',
+        'owner_pure_phone',
     )
-    readonly_fields = (
-        'created_at',
-    )
+    readonly_fields = ('created_at',)
     list_display = (
         'address',
         'price',
         'new_building',
         'construction_year',
         'town',
-        'owner_pure_phone'
+        'owner_pure_phone',
+        'active',
     )
-    list_editable = (
-        'new_building',
-    )
+    list_editable = ('new_building',)
     list_filter = (
         'new_building',
         'active',
         'has_balcony',
         'rooms_number',
-        'town',
     )
-    raw_id_fields = (
-        'liked_by',
-    )
+    raw_id_fields = ('liked_by', 'owners')
+
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
-    list_display = (
-        'user',
-        'flat',
-        'text'
-    )
-    raw_id_fields = (
-        'user',
-        'flat'
-    )
+    list_display = ('user', 'flat', 'text')
+    search_fields = ('user__username', 'flat__address')
+    raw_id_fields = ('user', 'flat')
+
